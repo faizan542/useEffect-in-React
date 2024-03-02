1c1d3412ad34e1092910adb1c89f5cf4a8cdf3ec
faizan542 <faizansilat100@gmail.com>
2024-03-02 19:49:14 +0500

    useEffect in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(0);
+
+
+  useEffect(() => {
+    alert("count was changed");
+    setColor(color + 1);
+  }, [count]);
+
 
 
   return (
     <>
-      
+      <Navbar color={"blue" + color} />
+      <div>
+        {count}
+        <button onClick={() => { setCount(count + 1) }}>click me</button>
+      </div>
     </>
   )
 }
